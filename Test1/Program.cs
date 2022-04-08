4aaf7b2d57eda86594805f639e63acf4cdbcadb4
chrisgit1234 <christeze@yahoo.com>
2022-04-09 00:05:48 +0100

    file

---
+++ Test1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            //int number = 23;
+            //Console.WriteLine("Number " + number + " is prime = " + IsNumberPrimeOrNot(number));
+        }
+
+
+        //public static bool IsNumberPrimeOrNot(int number)
+        //{
+        //    for (int i = 1; i <= number; i++)
+        //    {
+        //        if (number % i == 0)
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
+        //public static int NumberOfOnes(int number)
+        //{
+
+        //    return 0;
+
+            // Method returns true if number is prime, false if not
+            // Prime number is divisible by 1 and itself.
+            // E.g. 17 is prime
+            //      8 is not, as it can be divided by 1,2,4,8
+            // This method should return the number of 1's in the binary representation of the input number
+            // For eg, the binary representation of 21 is 10101. So this method should return 3
+
+        //}
+    }
+}
